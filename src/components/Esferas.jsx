50b28d800a07847910a6e7527fc66d72d643d9d2
Bruno Gimenes <bruno.gmouro@gmail.com>
2021-04-08 22:16:37 -0300

    first commit

---
+++ src/components/Esferas.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Esferas.css'
+
+function Esferas() {
+    return (
+        <div className = 'container'>
+            <span className="esfera" id='1'></span>
+            <span className="esfera" id='2'></span>
+            <span className="esfera" id='3'></span>
+            <span className="esfera" id='4'></span>            
+        </div>
+    )
+}
+
+export default Esferas
